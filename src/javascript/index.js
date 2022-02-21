7dc5a8793e75af4fd0521a0f48aeb2017a64e88a
nazimudheen267 <nazimudheen267@gmail.com>
2022-02-21 23:28:42 +0530

    nav-links added into nav-bar

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,15 @@
-['like','thank'].map(l => l);
+"use strict";
+
+const elements = document.querySelectorAll("#js-nav-links .nav__link_color");
+
+elements.forEach((element) => {
+	element.addEventListener("mouseover", (event) => {
+		const text = event.target.innerText.toLowerCase();
+		const eventElem = document.querySelector(`.${text}-list`);
+		eventElem.classList.add("animation-nav");
+
+		eventElem.addEventListener("mouseleave", () => {
+			eventElem.classList.remove("animation-nav");
+		});
+	});
+});
