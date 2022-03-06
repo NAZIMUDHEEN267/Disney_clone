08ad08201c4204b050b5746e1c6cd6eebb05cb2e
nazimudheen267 <nazimudheen267@gmail.com>
2022-03-06 16:06:08 +0530

    scrolling part just added

--- build/js/index.js
+++ build/js/index.js
@@ -1,5 +1,3 @@
-"use strict";
-
 // increasing the search input width
 
 const body = document.querySelector("body");
@@ -8,7 +6,7 @@ const carousel = document.getElementById("js-carousel");
 const btnLeft = document.getElementById("js-btn-left");
 const btnRight = document.getElementById("js-btn-right");
 
-body.addEventListener("click", event => {
+body.addEventListener("click", (event) => {
 	if (event.target.placeholder === "search") {
 		event.target.style.width = "100%";
 	} else {
@@ -16,43 +14,76 @@ body.addEventListener("click", event => {
 	}
 });
 
-// data fetching
-async function data() {
-	const jsonData = await fetch("/data/data.json");
-	const getData = await jsonData.json();
-	const { container } = getData;
+class Slider {
+	constructor() {
+		// data fetching
+		fetch("/data/data.json")
+			.then((jsonData) => jsonData.json())
+			.then((data) => {
+				// destructuring container
+				const { container } = data;
 
-	// data insertion in html
-	const path = "../Assets/container/";
-
-	for (let i = 0; i < container.length; i++) {
-		const modifiedName = container[i].name.split("_").join(" ");
-		carousel.innerHTML += `
-							<div class="container__item flex">
-								<div class="text">
-									<h1 class="headline">${modifiedName}</h1>
-									<p class="genre">thriller
-										<span class="rating">4.2</span>
-									</p>
-									<span class="about">
-										Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed reprehenderit excepturi enim sit laborum eveniet, ullam fugiat soluta eos aliquid, possimus odio deleniti nobis dignissimos sint assumenda eum. Quod, repudiandae.
-									</span>
-								</div>
-
-								<div class="img">
-									<img src="${path}${container[i].img}" alt=${container[i].img}/>
-								</div>
-							</div>
-						`;
+				this.items = container;
+			});
 	}
+
+	creator(name, image) {
+		// creating a new node
+		return `
+				<div class="container__item flex">
+					<div class="text">
+						<h1 class="headline">${name}</h1>
+						<p class="genre">thriller
+							<span class="rating">4.2</span>
+						</p>
+						<span class="about">
+							Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed reprehenderit excepturi enim sit laborum eveniet, ullam fugiat soluta eos aliquid, possimus odio deleniti nobis dignissimos sint assumenda eum. Quod, repudiandae.
+						</span>
+					</div>
+					<div class="img">
+						<img src="../Assets/container/${image}" alt=${image}/>
+					</div>
+				</div>
+			`;
+	}
+
+	// move slide functions
+	forward() {
+		carousel.innerHTML += this.creator(
+			this.items[this.increment].name,
+			this.items[this.increment].img
+		);
+
+		const children = carousel.children;
+
+		if (children.length > 2) {
+			carousel.innerHTML += this.creator(
+				this.items[this.increment + 1].name,
+				this.creator[this.increment + 1].img
+			)
+		}
+
+		// movement
+		const scrollTo = children[this.increment];
+
+		scrollTo.scrollIntoView(true);
+
+		increment += 1;
+	}
+
+	backward() {}
 }
 
-data();
+const slid = new Slider();
+// move forward slide, when clicking button right
+btnRight.addEventListener("click", () => slid.forward());
 
-// carousel effect
-btnLeft.addEventListener("click", e => {});
+// move backward slide when clicking left button
+// btnLeft.addEventListener("click", () => backward());
 
-let client = carousel.offsetWidth;
-btnRight.addEventListener("click", () => {
-	carousel.scrollBy(client, 10);
-});
+// // automatic slide movement
+// setInterval(() => {
+// 	if (i === carousel.children.length - 1) i = 0;
+// 	forward();
+// }, 7000);
+console.log('hello');
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -15,44 +15,78 @@ body.addEventListener("click", (event) => {
 	}
 });
 
-// data fetching
-async function data() {
-	const jsonData = await fetch("/data/data.json");
-	const getData = await jsonData.json();
-	const { container } = getData;
+class Slider {
+	constructor() {
+		// data fetching
+		fetch("/data/data.json")
+			.then((jsonData) => jsonData.json())
+			.then((data) => {
+				// destructuring container
+				const { container } = data;
 
-	// data insertion in html
-	const path = "../Assets/container/";
-
-	for (let i = 0; i < container.length; i++) {
-		const modifiedName = container[i].name.split("_").join(" ");
-		carousel.innerHTML += `
-							<div class="container__item flex">
-								<div class="text">
-									<h1 class="headline">${modifiedName}</h1>
-									<p class="genre">thriller
-										<span class="rating">4.2</span>
-									</p>
-									<span class="about">
-										Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed reprehenderit excepturi enim sit laborum eveniet, ullam fugiat soluta eos aliquid, possimus odio deleniti nobis dignissimos sint assumenda eum. Quod, repudiandae.
-									</span>
-								</div>
-
-								<div class="img">
-									<img src="${path}${container[i].img}" alt=${container[i].img}/>
-								</div>
-							</div>
-						`;
+				this.items = container;
+				this.increment = 1;
+			});
 	}
+
+	creator(name, image) {
+		// creating a new node
+		return `
+				<div class="container__item flex">
+					<div class="text">
+						<h1 class="headline">${name}</h1>
+						<p class="genre">thriller
+							<span class="rating">4.2</span>
+						</p>
+						<span class="about">
+							Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed reprehenderit excepturi enim sit laborum eveniet, ullam fugiat soluta eos aliquid, possimus odio deleniti nobis dignissimos sint assumenda eum. Quod, repudiandae.
+						</span>
+					</div>
+					<div class="img">
+						<img src="../Assets/container/${image}" alt=${image}/>
+					</div>
+				</div>
+			`;
+	}
+
+	// move slide functions
+	forward() {
+		
+		carousel.innerHTML += this.creator(
+			this.items[this.increment].name,
+			this.items[this.increment].img
+			);
+			
+			// get children's
+			const children = carousel.children;
+
+			if(children.length > 2){
+				children[0].remove();
+			}
+
+			// movement
+			const scrollTo = children[1];
+			scrollTo.scrollIntoView(true);
+
+			if (this.increment === this.items.length - 1) {
+				this.increment = -1;
+			}
+
+		this.increment += 1;
+	}
+
+	backward() {}
 }
 
-data();
+const slider = new Slider();
 
-// carousel effect
-btnLeft.addEventListener("click", (e) => {
-});
+// move forward slide, when clicking button right
+btnRight.addEventListener("click", () => slider.forward());
 
-let client = carousel.offsetWidth ;
-btnRight.addEventListener("click", () => {
-	carousel.scrollBy(client, 10)
-});
+// move backward slide when clicking left button
+// btnLeft.addEventListener("click", () => backward());
+
+// automatic slide movement
+setInterval(() => {
+	slider.forward();
+}, 7000);
